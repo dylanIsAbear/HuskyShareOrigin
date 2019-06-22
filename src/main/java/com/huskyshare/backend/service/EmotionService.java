d583bd9292f9fffd6e3727429f41bd82b6bbe912
Josh Tan <joshtan@192.168.0.103>
2019-06-22 15:13:44 +0800

    -emotion

--- src/main/java/com/huskyshare/backend/service/EmotionService.java
+++ src/main/java/com/huskyshare/backend/service/EmotionService.java
@@ -1,7 +1,37 @@
 package com.huskyshare.backend.service;
 
+import com.huskyshare.backend.dao.EmotionDao;
+import com.huskyshare.backend.entity.EmotionReply;
+import com.huskyshare.backend.entity.Emotion;
+import com.huskyshare.backend.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmotionService {
+    @Autowired
+    EmotionDao emotionDao;
+
+    /*
+    @Autowired
+    EmotionReply emotionReply;
+    */
+
+    public void save(Emotion emotion){
+        emotionDao.save(emotion);
+    }
+
+    public Emotion findById(Long id){
+        return emotionDao.findEmotionById(id);
+    }
+
+    public List<Emotion> findAllByUser (User user){
+        return emotionDao.findAllEmotionByUser(user);
+    }
+
+    public List<Emotion> findAllAvailableByUser(User user){
+        return emotionDao.findAllAvailableEmotion(user);
+    }
 }
