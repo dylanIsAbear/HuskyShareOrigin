088b7760ba9e369f9b377b25905e31f505873e39
dylanIsAbear <yuanhl3@uw.edu>
2019-07-05 22:37:09 +0800

    Emotion

--- src/main/java/com/huskyshare/backend/entity/Profile.java
+++ src/main/java/com/huskyshare/backend/entity/Profile.java
@@ -3,7 +3,6 @@ package com.huskyshare.backend.entity;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import javax.persistence.*;
 import java.util.Date;
 
